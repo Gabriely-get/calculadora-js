2dd431a389cb96585b5768faa920587ce707030b
Gabriely-get <gabrielysantos.informatica@gmail.com>
2023-09-21 01:14:33 -0300

    feat: calculator math operations and clear

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ const number = document.querySelectorAll(".number");
 const operation = document.querySelectorAll(".operation");
 const get_result = document.querySelector("#get-result");
 const display = document.querySelector("#display");
+const clear = document.querySelector("#clear");
 
-let result;
+let lastResult = 0;
+let result = 0;
 let valueBeforeOperationSymbol = 0;
 let valueAfterOperationSymbol = 0;
 let operationSetted;
 
+get_result.addEventListener("click", () => {
+    setDisplay(result);
+})
+
+clear.addEventListener("click", () => {
+    setDisplay('');
+    result = '';
+})
+
 function setDisplay(value) {
-    display.value += value;
+    display.value = value;
 }
 
 function getDisplay() {
@@ -19,9 +30,45 @@ function getDisplay() {
 for (let i = 0; i < number.length; i++) {
 
     number[i].addEventListener("click", () => {
-        setDisplay(number[i].value);
-    });
+        if (operationSetted) {
+            setDisplay('');
+        }
 
-    result = getDisplay();
-    operationSetted = false;
+        setDisplay(getDisplay() + number[i].value);
+        calc(operationSetted);
+        operationSetted = false;
+    });
+    
+}
+
+for (let i = 0; i < operation.length; i++) {
+
+    operation[i].addEventListener("click", () => {
+        operationSetted = operation[i].value;
+        setDisplay(result);
+    });
+    
+}
+
+function calc(isOperationSetted) {
+    if (isOperationSetted) {
+        if (isOperationSetted == "+") {
+            result = Number(result) + Number(getDisplay());
+        }
+        if (isOperationSetted == "-") {
+            result = Number(result) - Number(getDisplay());
+        }
+        if (isOperationSetted == "/") {
+            result = Number(result) / Number(getDisplay());
+        }
+        if (isOperationSetted == "*") {
+            result = Number(result) * Number(getDisplay());
+        }
+        if (isOperationSetted == "^") {
+            result = Math.pow(Number(result), Number(getDisplay()));
+        }
+    } else {
+        result = getDisplay();
+    }
+
 }
